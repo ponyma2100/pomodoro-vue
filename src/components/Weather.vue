8767b92baa3292f20f51db580992808528e674ec
Pony <ponyma2100@gmail.com>
2021-09-04 22:56:13 +0800

    Add getForecastWeather in getWeather.js

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -1,39 +1,210 @@
 <template>
   <div class="weather">
     <div class="weather-dash" @click="handleClick">
-      <div class="weather-data">
-        <span class="weather-icon">{{ currentWeatherData.description }}</span>
-        <span class="weather-temp">{{ currentWeatherData.temperature }}</span>
-        <span class="weather-degree">°</span>
+      <div class="weather-dash-data">
+        <span class="weather-dash-icon">{{
+          currentWeatherData.description
+        }}</span>
+        <span class="weather-dash-temp">{{
+          currentWeatherData.temperature
+        }}</span>
+        <span class="weather-dash-degree">°</span>
       </div>
       <div class="weather-location">
-        <span>{{ currentWeatherData.locationName }}</span>
+        <span>{{ cityName }}</span>
       </div>
     </div>
     <div class="weather-card" v-show="showCard">
-      <span>{{ currentWeatherData.locationName }}</span>
-      <span class="weather-icon">{{ currentWeatherData.description }}</span>
-      <span class="weather-temp">{{ currentWeatherData.temperature }}</span>
+      <div class="weather-card-location">
+        <form @change.prevent="handleSubmit(cityName)">
+          <select v-model="cityName">
+            <option
+              v-for="city in cityData"
+              :key="city.cityName"
+              :value="city.cityName"
+            >
+              {{ city.cityName }}
+            </option>
+          </select>
+        </form>
+        <span>{{ cityName }}</span>
+      </div>
+      <div class="weather-card-description">
+        <span class="weather-icon">{{ currentWeatherData.description }}</span>
+        <span class="weather-temp">{{ currentWeatherData.temperature }}</span>
+        <div class="weather-forecast">
+          <div
+            class="weather-maxt"
+            v-for="forecast in filterMaxT"
+            :key="forecast"
+          >
+            <span>{{ forecast.elementValue[0].value }}</span>
+          </div>
+          <div
+            class="weather-mint"
+            v-for="forecast in filterMinT"
+            :key="forecast"
+          >
+            <span>{{ forecast.elementValue[0].value }}</span>
+          </div>
+          <div
+            class="weather-description"
+            v-for="forecast in filterDescription"
+            :key="forecast"
+          >
+            <span>{{ forecast.elementValue[0].value }}</span>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import { ref } from "@vue/reactivity";
+import { computed, ref } from "@vue/reactivity";
 import getWeather from "../composables/getWeather";
 export default {
   setup() {
-    const { currentWeatherData, getCurrentWeather } = getWeather();
+    const {
+      currentWeatherData,
+      forecastWeatherData,
+      getCurrentWeather,
+      getForecastWeather,
+    } = getWeather();
     const showCard = ref(false);
+    const city = ref("臺北");
+    const cityData = ref([]);
+    const cityName = ref("臺北市");
+
     const handleClick = () => {
       showCard.value = !showCard.value;
-      console.log(
-        "🚀 ~ file: Weather.vue ~ line 26 ~ handleClick ~ showCard.value",
-        showCard.value
-      );
     };
-    getCurrentWeather();
-    return { currentWeatherData, handleClick, showCard };
+    2;
+
+    const handleSubmit = (cityName) => {
+      console.log(
+        "🚀 ~ file: Weather.vue ~ line 86 ~ handleSubmit ~ cityName",
+        cityName
+      );
+      getForecastWeather(cityName);
+
+      cityData.value.filter((city) => {
+        if (city.cityName === cityName) {
+          city.value = city.locationName;
+          getCurrentWeather(city.value);
+          return city.locationName;
+        }
+      });
+    };
+
+    getCurrentWeather(city.value);
+    getForecastWeather(cityName.value);
+
+    const filterMaxT = computed(() => {
+      return forecastWeatherData.value[0];
+    });
+    const filterMinT = computed(() => {
+      return forecastWeatherData.value[1];
+    });
+    const filterDescription = computed(() => {
+      return forecastWeatherData.value[2];
+    });
+
+    cityData.value = [
+      {
+        cityName: "宜蘭縣",
+        locationName: "宜蘭",
+      },
+      {
+        cityName: "嘉義市",
+        locationName: "嘉義",
+      },
+      {
+        cityName: "屏東縣",
+        locationName: "恆春",
+      },
+      {
+        cityName: "雲林縣",
+        locationName: "麥寮",
+      },
+      {
+        cityName: "臺東縣",
+        locationName: "臺東",
+      },
+      {
+        cityName: "臺北市",
+        locationName: "臺北",
+      },
+      {
+        cityName: "金門縣",
+        locationName: "金門",
+      },
+      {
+        cityName: "桃園市",
+        locationName: "新屋",
+      },
+      {
+        cityName: "彰化縣",
+        locationName: "彰師大",
+      },
+      {
+        cityName: "嘉義縣",
+        locationName: "嘉義",
+      },
+      {
+        cityName: "高雄市",
+        locationName: "高雄",
+      },
+      {
+        cityName: "基隆市",
+        locationName: "基隆",
+      },
+      {
+        cityName: "臺南市",
+        locationName: "南區中心",
+      },
+      {
+        cityName: "南投縣",
+        locationName: "日月潭",
+      },
+      {
+        cityName: "臺中市",
+        locationName: "臺中",
+      },
+      {
+        cityName: "新竹縣",
+        locationName: "新竹",
+      },
+      {
+        cityName: "花蓮縣",
+        locationName: "花蓮",
+      },
+      {
+        cityName: "連江縣",
+        locationName: "馬祖",
+      },
+      {
+        cityName: "澎湖縣",
+        locationName: "澎湖",
+      },
+      {
+        cityName: "新北市",
+        locationName: "板橋",
+      },
+    ];
+    return {
+      currentWeatherData,
+      handleClick,
+      showCard,
+      city,
+      handleSubmit,
+      forecastWeatherData,
+      filterMaxT,
+      filterMinT,
+      filterDescription,
+      cityData,
+      cityName,
+    };
   },
 };
 </script>
@@ -55,9 +226,13 @@ span {
   border-radius: 20px;
   background: white;
   text-align: center;
-  padding: 110px 0;
   margin: 5px;
   align-self: flex-end;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  height: 100%;
+  padding: 1rem;
 }
 
 .weather-card span {
--- src/composables/getWeather.js
+++ src/composables/getWeather.js
@@ -1,16 +1,20 @@
 import { ref } from "vue"
 
 const authorization = 'CWB-042511E5-4B1B-4F0B-B0E0-678048C3471E'
-const apiUrl = `https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization=${authorization}&locationName=臺北`
 
 
 const getWeather = () => {
   const locationData = ref([])
   const currentWeatherData = ref({})
+  const forecastWeatherData = ref({})
 
-  const getCurrentWeather = async () => {
+
+  const getCurrentWeather = async (city) => {
+    const base = `https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0003-001`
+    const query = `?Authorization=${authorization}&locationName=${city}`
+
     try {
-      const res = await fetch(apiUrl)
+      const res = await fetch(base + query)
       const data = await res.json()
 
       locationData.value = data.records.location[0]
@@ -34,8 +38,44 @@ const getWeather = () => {
     }
   }
 
-  return { currentWeatherData, getCurrentWeather }
+  const getForecastWeather = async (city) => {
+    const base = `https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-091`
+    const query = `?Authorization=${authorization}&locationName=${city}`
+    const locationData = ref([])
 
+    try {
+      const response = await fetch(base + query)
+      const data = await response.json()
+      locationData.value = data.records.locations[0].location[0].weatherElement
+
+      const weatherElements = locationData.value.reduce((needElements, item) => {
+        if (['MaxT', 'MinT', 'Wx'].includes(item.elementName)) {
+          const weekWeather = item.time.filter(element => {
+            return element.startTime.includes('06:00')
+          })
+          needElements[item.elementName] = weekWeather
+        }
+
+        return needElements
+      }, [])
+
+
+      forecastWeatherData.value = [
+        weatherElements.MaxT,
+        weatherElements.MinT,
+        weatherElements.Wx
+      ]
+
+
+      // console.log("🚀 ~ file: getWeather.js ~ line 86 ~ getForecastWeather ~ forecastWeatherData.value", forecastWeatherData.value)
+
+    } catch (error) {
+      console.log(error)
+    }
+
+
+  }
+  return { currentWeatherData, getCurrentWeather, getForecastWeather, forecastWeatherData }
 }
 
 
